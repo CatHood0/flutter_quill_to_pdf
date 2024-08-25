c140fd09f94d6ea8d6d8797c8c4b447a39951ab2
CatHood0 <santiagowmar@gmail.com>
2024-08-25 01:06:29 -0400

    Chore: removed useless constants values

--- lib/core/constant/constants.dart
+++ lib/core/constant/constants.dart
@@ -5,9 +5,6 @@ class Constant {
   static const String DEFAULT_FONT_FAMILY = 'Arial';
   //to encode markdown characters to avoid detection
   static const List<double> default_heading_size = <double>[37, 30, 24, 18, 12];
-  static final RegExp newLinesInsertions = RegExp(r'^¶+');
-  static final RegExp rawDeltaNewLines = RegExp(r'^\\n{1,}(?!(.+?))');
-  //TODO: implement this images regex to improve support of network and local images (and base64)
   static final RegExp IMAGE_LOCAL_STORAGE_PATH_PATTERN =
       RegExp(r'^((\/[a-zA-Z0-9-_]+)+|\/)$');
   static final RegExp IMAGE_FROM_NETWORK_URL = RegExp(
