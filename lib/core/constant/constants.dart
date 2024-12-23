d54bd423a37150119b7d9c7ce0c28a8684ed3b45
CatHood0 <santiagowmar@gmail.com>
2024-12-23 19:13:52 -0400

    Fix: image_local_path_pattern was improved

--- lib/core/constant/constants.dart
+++ lib/core/constant/constants.dart
@@ -3,11 +3,12 @@ class Constant {
   static const double DEFAULT_LINE_HEIGHT = 1.0;
   static const int DEFAULT_FONT_SIZE = 12;
   static const String DEFAULT_FONT_FAMILY = 'Arial';
-  //to encode markdown characters to avoid detection
+  // to encode markdown characters to avoid detection
   static const List<double> default_heading_size = <double>[37, 30, 24, 18, 12];
   static final RegExp newLinesInsertions = RegExp(r'^¶+');
+  // only valid for mobile devices
   static final RegExp IMAGE_LOCAL_STORAGE_PATH_PATTERN =
-      RegExp(r'^((\/[a-zA-Z0-9-_]+)+|\/)$');
+      RegExp(r'^((\/[a-zA-Z0-9-_]+)+|\/)(\..+?)?$');
   static final RegExp IMAGE_FROM_NETWORK_URL = RegExp(
       r'^http(s)?(?:(?<scheme>[^:\/?#]+):)?(?:\/\/(?<authority>[^\/?#]*))?(?<path>[^?#]*\/)?(?<file>[^?#]*\.(?<extension>[Jj][Pp][Ee]?[Gg]|[Pp][Nn][Gg]|[Gg][Ii][Ff]))(?:\?(?<query>[^#]*))?(?:#(?<fragment>.*))?$');
 }
