b3e6371d7205dccf482681a9620a8ddb741af413
CatHood0 <santiagowmar@gmail.com>
2025-03-03 20:05:42 -0400

    Chore: added docs about items of ListTypeWidget enum

--- lib/src/core/enums/list_type_widget.dart
+++ lib/src/core/enums/list_type_widget.dart
@@ -7,6 +7,14 @@
 ///     It's called modern, because this implementation does it's more exact than the versions used in DOCX or another editors 
 ///     _Since this version is not stable, can cause rendering errors if the widget overlaps the entire page height_ 
 enum ListTypeWidget {
+  /// decides that the builded widget will be the old version
+  ///
+  /// It's called "stable", because this implementation does not cause rendering errors 
+  /// if the widgets overlaps the height of the page
   stable,
+  /// decides that the builded widget will be new version 
+  /// It's called modern, because this implementation does it's more exact than the versions used in DOCX or another editors 
+  ///
+  /// _Since this version is not stable, can cause rendering errors if the widget overlaps the entire page height_ 
   modern,
 }
